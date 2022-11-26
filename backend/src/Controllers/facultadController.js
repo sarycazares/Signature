40486b5579bafc1339002dd8cb0daa13b0621c86
mileswolf22 <milestom22@hotmail.com>
2022-11-26 05:46:44 -0600

    Actualizacion pre - Manejo correcto de agregaciones

--- backend/src/Controllers/facultadController.js
+++ backend/src/Controllers/facultadController.js
@@ -4,7 +4,7 @@ const Facultad = require("../models/facultadSchema");
 exports.facultad_create = async(req, res) => {
     const{body: facultad} = req;
 
-    const facultadDB = new Usuario(facultad);
+    const facultadDB = new Facultad(facultad);
     //const err = await facultadDB.validate();
 
     /*if(err.errors) {
@@ -16,7 +16,7 @@ exports.facultad_create = async(req, res) => {
       }*/
       console.log("sale de compilacion")
       await facultadDB.save().catch((err) => console.log("Algo ha fallado", err));
-
+      console.log("se agrego")
       /*const universidadDB = Universidad.findOne({ _id: UniversidadDB.universidad });
       const universidadSaved = Universidad.findOneAndUpdate(
         { _id: universidadDB.universidad },
@@ -34,12 +34,13 @@ exports.facultad_update = async(req, res) => {
 const {body:facultad} = req;
 
 try{
-let facultadDB = await Facultad.find({_id: facultad._id});
+let facultadDB = await Facultad.find({faultad_nombre: facultad.facultad_nombre});
 if(facultadDB){
-    const{nombre, siglas, universidad, ciudad} = facultad;
+  console.log("se encontro");
+    const{facultad_nombre} = facultad;
     const data = await Facultad.updateOne(
-        {nombre},
-        {nombre, siglas, universidad, ciudad}
+        {facultad_nombre},
+        {facultad_nombre}
     );
     res.send(data);
 }
@@ -48,6 +49,36 @@ res.send({ msg: "Facultad no encontrada" });
 console.log(err);
 };
 };
+
+exports.facultad_update2 = async(req, res)=>{
+  const { id } = req.params; // Los params son los que se envían en el URL.
+  const { body } = req; // Traigo mi objeto. Al { variable } se le llama destructuring y me sirve para acceder a las partes de mi objeto
+console.log("entra");
+
+  try{
+    console.log("entra otra vez");
+    const facultadDB = await Facultad.facultad_getById(id); // Hago una consulta basándome en el id. 
+    console.log("entra otra vez");
+    if(facultadDB){
+      console.log("encuentra");
+        // Actualizar el contenido de mi fila.
+        const data = await User.findOneAndUpdate(  
+        // El id en mi base de datos se guarda con un _
+            {facultad_nombre: id}, // El primer parámetro es un objeto ya que está entre { }, aquí se guardan los criterios de búsqueda. 
+            body, // El segundo parámetro es un objeto con los campos que se van a actualizar. 
+            {returnOriginal: false}) // Query Options. Este sirve para que me muestre la data nueva y no la anterior.
+        
+        res.send({message: "Data updated correctly", data}); // data: data <-- es ambiguo, por lo tanto es lo mismo solo poner data
+    }else{
+      console.log("no encuentra")
+        // Regresar un mensaje de error. 
+        res.send({message: "The id does not exists"});
+    }
+}catch(err){
+  console.log("falla")
+    res.send(err);
+}
+}
 
 exports.facultad_delete = async(req, res) => {
     try {
@@ -67,17 +98,34 @@ exports.facultad_delete = async(req, res) => {
 };
 
 exports.facultad_getById = async(req, res) =>{
-    const {
-        params: { id },
-      } = req;
-    
-      const data = await Facultad.findOne({ _id: id }).populate("universidad");
-    
+  const { id } = req.params;
+  // Método optimizado para buscar por ids.
+  const data = await User.findById(id); // Encuentra el primer registro que coincide con la condición. 
+  //const data = await User.findOne({_id: id}); // Es lo mismo que hacer lo de arriba
+
+  if(data){
       res.send(data);
+  }else{
+      res.send({message: "Facultad no existe."})
+  }
 };
 
 exports.facultad_getAll = async(req, res) =>{
-    const data = await Facultad.find().populate("universidad");
+  
+    const data = await Facultad.find();
 
     res.send(data);
+};
+
+exports.facultad_getByName = async(req, res) =>{
+  const { nombre_facultad } = req.params;
+  // Método optimizado para buscar por ids.
+  const data = await User.findById(nombre_facultad); // Encuentra el primer registro que coincide con la condición. 
+  //const data = await User.findOne({_id: id}); // Es lo mismo que hacer lo de arriba
+
+  if(data){
+      res.send(data);
+  }else{
+      res.send({message: "Facultad no existe."})
+  }
 };
